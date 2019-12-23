de93f4c7d142fa19767bbb1b24fd018f70dbfb52
chengy <chengy@hqhop.com>
2019-12-23 16:35:49 +0800

    mod # 把审批流程码修改到配置文件中

--- eladmin-system/src/main/java/com/hqhop/config/dingtalk/DingTalkConstant.java
+++ eladmin-system/src/main/java/com/hqhop/config/dingtalk/DingTalkConstant.java
@@ -1,6 +1,7 @@
 package com.hqhop.config.dingtalk;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
@@ -8,12 +9,19 @@ import javax.annotation.PostConstruct;
  * 定义钉钉常量
  * @author chengy
  */
+@Configuration
 public class DingTalkConstant {
 
     public static String CORPID;
     public static String AGENTID;
     public static String APPKEY ;
     public static String APPSECRET;
+
+    /** 物料基本档案管理审批 **/
+    public  static String PROCESSCODE_MATERIAL_ADD ;
+
+    /** 物料生产档案管理审批 **/
+    public  static String PROCESSCODE_MATERIALPRODUCTION_ADD ;
 
     @Value("${dingtalk.corpid}")
     public String corpid;
@@ -27,22 +35,18 @@ public class DingTalkConstant {
     @Value("${dingtalk.agentid}")
     public String agentid;
 
+    @Value("${dingtalk.processcode.material-base-add}")
+    public String processcodeMaterialBaseAdd;
+    @Value("${dingtalk.processcode.material-base-add}")
+    public String processcodeMaterialProductionAdd;
+
     @PostConstruct
     public void init() {
         this.CORPID = this.corpid;
         this.AGENTID = this.agentid;
         this.APPKEY = this.appkey;
         this.APPSECRET = this.appsecret;
+        this.PROCESSCODE_MATERIAL_ADD = this.processcodeMaterialBaseAdd;
+        this.processcodeMaterialProductionAdd = this.processcodeMaterialProductionAdd;
     }
-
-    /*
-    * 物料基本档案管理审批
-    * */
-    public  static final String PROCESSCODE_MATERIAL_ADD = "PROC-B0A45D0A-A9FC-43AE-BD86-76F54B5902AA";
-
-    /*
-     * 物料生产档案管理审批
-     * */
-    public  static final String PROCESSCODE_MATERIALPRODUCTION_ADD = "PROC-B0A45D0A-A9FC-43AE-BD86-76F54B5902AA";
-
 }
